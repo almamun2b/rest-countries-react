a4295b163ab24d5e5c2478b9c08d5abc9852bb45
mycodinglab <almamun2b@gmail.com>
2021-09-21 23:20:18 +0600

    Completed

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p><small>Copyright, Protected By Mamun</small></p>
+        </div>
+    );
+};
+
+export default Footer;
